badfa7b2d550b5c7516b39990f66fe786f4d5058
Constantine Zorge <wolverineo250kr@yandex.ru>
2023-07-17 04:10:26 +0500

    Update process_birthday.php

--- process_birthday.php
+++ process_birthday.php
@@ -2,13 +2,25 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require_once 'functions.php';
 
-    // Получаем значение даты рождения из формы
-    $birthday = $_POST['birthday'];
-    $currentUser = getCurrentUser();
-    setUserBirthday($birthday, $currentUser);
+    $dateString = $_POST['birthday'];
+    $format = 'Y-m-d';
 
-    // Перенаправляем пользователя на главную
-    die("<script>location.href = '/'</script>");
-    exit;
+    $date = DateTime::createFromFormat($format, $dateString);
+
+    if ($date && $date->format($format) === $dateString && strlen($dateString) === 10) {
+        // Дата прошла валидацию
+        // Получаем значение даты рождения из формы
+        $birthday = $_POST['birthday'];
+        $currentUser = getCurrentUser();
+        setUserBirthday($birthday, $currentUser);
+
+        // Перенаправляем пользователя на главную
+        die("<script>location.href = '/'</script>");
+        exit;
+    } else {
+        // Дата не прошла валидацию
+        echo "Некорректный формат даты рождения.";
+        die();
+    } 
 }
 ?>
